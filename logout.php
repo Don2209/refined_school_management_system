20a489f330219ca1bea1ba91dd35a9620a0788b2
Don2209 <rufarodon@gmail.com>
2025-04-05 21:10:36 -0500

    23

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+// Start session
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Destroy the session
+session_unset();
+session_destroy();
+
+// Redirect to the login page
+header('Location: index.php');
+exit;
+?>
